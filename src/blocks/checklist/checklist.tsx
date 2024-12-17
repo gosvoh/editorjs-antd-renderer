20ed1ec55ad403d387777ea87d47043383e9f978
Aleksey Vokhmin <vohmina2011@yandex.ru>
2024-12-17 22:27:26 +0300

    Refactor Checklist component to accept data as an object with items array

--- src/blocks/checklist/checklist.tsx
+++ src/blocks/checklist/checklist.tsx
@@ -5,11 +5,11 @@ import { Checkbox, Space } from "antd";
 export default function Checklist({
   data,
 }: {
-  data: { text: string; checked: boolean }[];
+  data: { items: { text: string; checked: boolean }[] };
 }) {
   return (
     <Space direction="vertical">
-      {data.map((item, index) => (
+      {data.items.map((item, index) => (
         <Checkbox key={`checklist-item-${index}`} checked={item.checked}>
           {parse(sanitizeHtml(item.text))}
         </Checkbox>
