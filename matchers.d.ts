894409cc82a5020d6764b18a164e7442e73fa04c
Aleksey Vokhmin <vohmina2011@yandex.ru>
2026-05-13 22:55:24 +0300

    Fix lint configuration for Windows CRLF and unused imports
    - Add endOfLine: auto to prettier config to handle Windows CRLF checkouts
    - Remove unused Matchers/AsymmetricMatchers imports from matchers.d.ts
    - Add eslint-disable for empty object type interfaces in matchers.d.ts

--- matchers.d.ts
+++ matchers.d.ts
@@ -1,9 +1,10 @@
 import { TestingLibraryMatchers } from "@testing-library/jest-dom/matchers";
-import { Matchers, AsymmetricMatchers } from "bun:test";
 
 declare module "bun:test" {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface Matchers<T>
     extends TestingLibraryMatchers<typeof expect.stringContaining, T> {}
   // @ts-expect-error https://github.com/oven-sh/bun/issues/15222
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface AsymmetricMatchers extends TestingLibraryMatchers {}
 }
--- prettier.config.js
+++ prettier.config.js
@@ -2,4 +2,6 @@
  * @see https://prettier.io/docs/en/configuration.html
  * @type {import("prettier").Config}
  */
-export default {};
+export default {
+  endOfLine: "auto",
+};
